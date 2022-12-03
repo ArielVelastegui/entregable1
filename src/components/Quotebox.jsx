07cdb34b81946afbcb8fadd1e6c1ec0b8b01cfe8
Ariel Velastegui <velastegui.ariel@gmail.com>
2022-12-03 11:14:02 -0500

    fixing img

--- src/components/Quotebox.jsx
+++ src/components/Quotebox.jsx
@@ -7,7 +7,7 @@ const objStyle = {color:color}
   return (
     <article className='quoteBox'>
         
-        <p style={objStyle}><img src="public/feather.png" alt="pluma" />{quote.quote}</p>
+        <p style={objStyle}><img src="image/feather.png" alt="pluma" />{quote.quote}</p>
         <h1 className='auth' style={objStyle}>{quote.author}</h1>
         <Buttons Randie={Randie} color={color}/>
     </article>
